ae795726b807d0967dc72a3207cd258a52ab045c
tartan2590 <tartan01@gmail.com>
2025-08-12 11:49:20 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 // Example (looks like):
 // https://docs.google.com/spreadsheets/d/e/XXXXXXXXXXXXXXXXXXXX/pub?gid=0&single=true&output=csv
 // For first run, we default to the local sample CSV. Replace this with your Sheet later.
-export const SHEET_CSV_URL = '/sample.csv'
+export const SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRFwCMjJtufYNqYzxpGlO-rfDjrxrNPyJZimAQVSHZ9rdd_x_jwrYoMtZR14yS2SNK3OOTuIiv0ASVQ/pub?output=csv'
 
 // Optional: for GitHub Pages, set this to your repo name (used in vite.config base)
 export const REPO_NAME = 'REPO_NAME'
